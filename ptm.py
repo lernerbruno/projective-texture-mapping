732e9ebe03c8deb7d5f58af02954b0eaa47953ff
BrunoLerner <bru.lerner@gmail.com>
2018-09-29 11:39:53 -0300

    working

--- ptm.py
+++ ptm.py
@@ -4,33 +4,24 @@ import matplotlib.pyplot as plt
 
 baseImg = cv2.imread("base.jpg")
 textureImg = cv2.imread("texture.png")
-
 bRows,bCols,ch = baseImg.shape
 tRows,tCols,ch = textureImg.shape
+print ch
 print bRows, bCols
-bImgPts = np.float32([[517,585],[1029,685],[1000,1600],[588,1600]])
-tImgPts = np.float32([[0,0],[tCols,0],[tCols,tRows],[0,tRows]])
+bImgPts = np.float32([[696.78,615.42],[696.78,1008.342],[1640.04,984.672],[1630.56,577.548]])
+tImgPts = np.float32([[0,0],[0,tCols],[tRows,tCols],[tRows,0]])
 M = cv2.getPerspectiveTransform(bImgPts,tImgPts)
 print "Transformation Matrix"
 print M
-print bImgPts[0][0]
+
 # Para cada ponto da imagem base, verificar se a transformada dele pertence ao suporte da textura
 for i in range(bRows):
     for j in range(bCols):
-        xT = M[0][0]*i + M[0][1]*j + M[0][2]
-        yT = M[1][0]*i + M[1][1]*j + M[1][2]
+        xT = (M[0][0]*i + M[0][1]*j + M[0][2])/(M[2][0]*i + M[2][1]*j + M[2][2]) 
+        yT = (M[1][0]*i + M[1][1]*j + M[1][2])/ (M[2][0]*i + M[2][1]*j + M[2][2]) 
         if (xT > 0 and xT < tRows) and (yT > 0 and yT < tCols):
-            print i,j
-            print baseImg[i][j], textureImg[int(xT)][int(yT)]
             baseImg[i][j] = textureImg[int(xT)][int(yT)]
             
         
-
-# Caso pertenca, substituir os pixels
-
-
-# dst = cv2.warpPerspective(bImgPts,M,(tCols,tRows))
-
-plt.subplot(121),plt.imshow(baseImg),plt.title('Texture')
-# plt.subplot(122),plt.imshow(dst),plt.title('Base image')
+plt.imshow(baseImg),plt.title('Texture in base image')
 plt.show()
