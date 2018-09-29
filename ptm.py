8064dcd5c80358b1d176adee0f75431bcd4ea4bc
BrunoLerner <bru.lerner@gmail.com>
2018-09-28 21:08:35 -0300

    Now doing linear transformation and trading colors

--- ptm.py
+++ ptm.py
@@ -7,13 +7,30 @@ textureImg = cv2.imread("texture.png")
 
 bRows,bCols,ch = baseImg.shape
 tRows,tCols,ch = textureImg.shape
-
-bImgPts = np.float32([[617,685],[1029,685],[1000,1600],[588,1600]])
+print bRows, bCols
+bImgPts = np.float32([[517,585],[1029,685],[1000,1600],[588,1600]])
 tImgPts = np.float32([[0,0],[tCols,0],[tCols,tRows],[0,tRows]])
+M = cv2.getPerspectiveTransform(bImgPts,tImgPts)
+print "Transformation Matrix"
+print M
+print bImgPts[0][0]
+# Para cada ponto da imagem base, verificar se a transformada dele pertence ao suporte da textura
+for i in range(bRows):
+    for j in range(bCols):
+        xT = M[0][0]*i + M[0][1]*j + M[0][2]
+        yT = M[1][0]*i + M[1][1]*j + M[1][2]
+        if (xT > 0 and xT < tRows) and (yT > 0 and yT < tCols):
+            print i,j
+            print baseImg[i][j], textureImg[int(xT)][int(yT)]
+            baseImg[i][j] = textureImg[int(xT)][int(yT)]
+            
+        
 
-M = cv2.getPerspectiveTransform(tImgPts, bImgPts)
-dst = cv2.warpPerspective(textureImg,M,(bRows,bCols))
+# Caso pertenca, substituir os pixels
 
-plt.subplot(121),plt.imshow(textureImg),plt.title('Texture')
-plt.subplot(122),plt.imshow(dst),plt.title('Base image')
+
+# dst = cv2.warpPerspective(bImgPts,M,(tCols,tRows))
+
+plt.subplot(121),plt.imshow(baseImg),plt.title('Texture')
+# plt.subplot(122),plt.imshow(dst),plt.title('Base image')
 plt.show()
